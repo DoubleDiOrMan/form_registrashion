b431f358c5b9b830fc4293b95159502e98334334
DoubleDiOrMan <92528600+DoubleDiOrMan@users.noreply.github.com>
2022-05-23 21:01:13 +0300

    Загрузил папку проекта
    Тут используется БД

---
+++ form_regist/Лаба20_Курьяков_319-4/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Лаба20_Курьяков_319_4
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection mycon;
+        public Form1()
+        {
+            InitializeComponent();
+            string s = @"Data Source = DESKTOP-E8UQAJI\SQLEXPRESS; Initial catalog = Lab21_Kuryakov_319/4; Integrated Security = True";
+            mycon = new SqlConnection(s);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kia = "select count(*) from users where login = @login and hashpass = HASHBYTES('SHA2_256', '" + textBox2.Text +"')";
+            string kia1 = "select name from users where login = @login";
+            string kia2 = "select count(*) from users where login = @login and role = 'admin'";
+            SqlParameter p1 = new SqlParameter("@login", textBox1.Text);
+            SqlParameter p2 = new SqlParameter("@login", textBox1.Text);
+            SqlParameter p3 = new SqlParameter("@login", textBox1.Text);
+            SqlCommand cm1 = new SqlCommand(kia, mycon);
+            SqlCommand cm2 = new SqlCommand(kia1, mycon);
+            SqlCommand cm3 = new SqlCommand(kia2, mycon);
+            cm1.Parameters.Add(p1);
+            cm2.Parameters.Add(p2);
+            cm3.Parameters.Add(p3);
+
+
+            mycon.Open();
+            if (Convert.ToInt32(cm1.ExecuteScalar()) == 1)
+            {
+                if (Convert.ToInt32(cm3.ExecuteScalar()) == 1) { 
+                    MessageBox.Show("Привет, админ " + Convert.ToString(cm2.ExecuteScalar()) + "!", "Добро пожаловать!");
+                    Form3 f3 = new Form3();
+                    f3.Show();
+                    this.Hide();
+                }
+                else { 
+                    MessageBox.Show("Привет, обычный смертный " + Convert.ToString(cm2.ExecuteScalar()) + "!", "Добро пожаловать!");
+                    Form4 f4 = new Form4();
+                    f4.Show();
+                    this.Hide();
+                }
+
+            }
+            else
+            {
+                if ((textBox1.Text == "") || (textBox2.Text == "")) {
+                    MessageBox.Show("Что же ты тут делаешь тогда?", "Я рад, что ты даже не пытаешься");
+                }
+                else {
+                    MessageBox.Show("Логин/Пароль НеВерный!!!", "Как так-то, ну...");
+                }
+                
+            }
+            mycon.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ form_regist/Лаба20_Курьяков_319-4/Form2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Лаба20_Курьяков_319_4
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection mycon;
+        public Form2()
+        {
+            InitializeComponent();
+            string s = @"Data Source = DESKTOP-E8UQAJI\SQLEXPRESS; Initial catalog = Lab21_Kuryakov_319/4; Integrated Security = True";
+            mycon = new SqlConnection(s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ass = "insert into users values ('" + textBox1.Text + "', '" + textBox2.Text + "', 'user', HASHBYTES('SHA2_256', '" + textBox3.Text + "'))";
+            string ass1 = "select count(*) from users where login = @login";
+            SqlParameter p1 = new SqlParameter("@login", textBox1.Text);
+            SqlCommand cm = new SqlCommand(ass1, mycon);
+            SqlCommand cm1 = new SqlCommand(ass, mycon);
+            cm.Parameters.Add(p1);
+
+            mycon.Open();
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == ""))
+            {
+                MessageBox.Show("Поля должны быть заполнены!", "Айяйяй");
+            }
+            else
+            {
+                if (Convert.ToInt32(cm.ExecuteScalar()) == 0)
+                {
+                    if (Convert.ToInt32(cm1.ExecuteNonQuery()) == 1)
+                    {
+                        MessageBox.Show("К сожалению, вы зарегестрировались", "Успех?!");
+                        Form1 f1 = new Form1();
+                        f1.Show();
+                        this.Hide();
+                    }
+                   
+                        
+                    
+                }
+                else
+                {
+                    MessageBox.Show("К счастью, такой логин занят)", "ОЙойой...");
+                }
+            }
+            mycon.Close();
+        }
+    }
+}
